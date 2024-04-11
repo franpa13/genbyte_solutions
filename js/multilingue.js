fcb84e917d917be23e7f8c9dff9a35651cb80c80
Lautaro073 <lautarojimene02@gmail.com>
2024-04-11 01:37:26 -0300

    funcion de traduccion de pagina

---
+++ js/multilingue.js
@@ -0,0 +1,224 @@
+const traducciones = {
+    'home': {
+      'en': 'Home',
+      'es': 'Inicio'
+    },
+    'about': {
+      'en': 'About',
+      'es': 'Acerca de'
+    },
+    'services': {
+      'en': 'Services',
+      'es': 'Servicios'
+    },
+    'showcase': {
+      'en': 'Showcase',
+      'es': 'Portafolio'
+    },
+    'team': {
+      'en': 'Team',
+      'es': 'Equipo'
+    },
+    'contact': {
+      'en': 'Contact',
+      'es': 'Contacto'
+    },
+    'idioma': {
+        'en': '<img src="./img/icons/en.svg" alt="English" /> 🇺🇸',
+        'es': '<img src="./img/icons/es.svg" alt="Español" /> 🇪🇸'
+      },
+      'headTitle': {
+        'en': 'BEGIN THE <span class="digital-transform">DIGITAL TRANSFORMATION</span> OF YOUR COMPANY TODAY!',
+        'es': 'INICIA LA <span class="digital-transform">TRANSFORMACIÓN DIGITAL</span> DE TU EMPRESA HOY!'
+      },
+      'introParagraph': {
+        'en': "Start shaping the future of your company today through our expertise in digital transformation. We guide you towards innovative and strategic technological solutions that will enhance your competitiveness and prepare you for tomorrow\'s challenges",
+        'es': "Comienza a dar forma al futuro de tu empresa hoy a través de nuestra experiencia en transformación digital. Te guiamos hacia soluciones tecnológicas innovadoras y estratégicas que mejorarán tu competitividad y te prepararán para los desafíos de mañana"
+      },
+      'serviceTitle1': {
+        'en': 'Genbyte Solutions',
+        'es': 'Soluciones Genbyte'
+      },
+      'serviceDesc1': {
+        'en': 'We are proudly dedicated to providing first-class services in software development and maintenance.',
+        'es': 'Nos dedicamos con orgullo a proporcionar servicios de primera clase en desarrollo y mantenimiento de software.'
+      },
+      'serviceTitle2': {
+        'en': 'Human Capital Provision',
+        'es': 'Provisión de Capital Humano'
+      },
+      'serviceDesc2': {
+        'en': 'In our Human Capital presentation, we are committed to meeting the specialized needs of our clients in terms of personnel.',
+        'es': 'En nuestra prestación de Capital Humano, estamos comprometidos a satisfacer las necesidades especializadas de nuestros clientes en términos de personal'
+      },
+      'serviceTitle3': {
+        'en': 'Our Commitment',
+        'es': 'Nuestro Compromiso'
+      },
+      'serviceDesc3': {
+        'en': 'Our highly skilled team strives to provide high-quality service that responds promptly to the demands of the current market.',
+        'es': 'Nuestro equipo altamente calificado se esfuerza por proporcionar un servicio de alta calidad que responda rápidamente a las demandas del mercado actual.'
+      },
+      'whatWeDoTitle': {
+        'en': 'What do we do?',
+        'es': '¿Qué hacemos?'
+      },
+      'whatWeDoDesc': {
+        'en': 'We turn your ideas into digital solutions. Our highly skilled team is dedicated to creating custom software that drives efficiency, innovation, and growth for your business.',
+        'es': 'Convertimos tus ideas en soluciones digitales. Nuestro equipo altamente calificado se dedica a crear software personalizado que impulse la eficiencia, innovación y crecimiento de tu negocio.'
+      },
+      'servicesProvideTitle': {
+        'en': 'Services We Provide',
+        'es': 'Servicios Que Ofrecemos'
+      },
+      'responsiveDesignsTitle': {
+        'en': 'Responsive designs',
+        'es': 'Diseños adaptables'
+      },
+      'responsiveDesignsDesc': {
+        'en': 'Our designs adapt perfectly to different devices and screens, providing a smooth and engaging user experience on any platform',
+        'es': 'Nuestros diseños se adaptan perfectamente a diferentes dispositivos y pantallas, proporcionando una experiencia de usuario fluida y atractiva en cualquier plataforma.'
+      },
+      'cleanScalableCodeTitle': {
+        'en': 'Clean & Scalable Code',
+        'es': 'Código Limpio y Escalable'
+      },
+      'cleanScalableCodeDesc': {
+        'en': 'We are committed to maintaining clean and structured code enabling continuous development and easy scalability of your custom software.',
+        'es': 'Estamos comprometidos con mantener un código limpio y estructurado que permita el desarrollo continuo y la escalabilidad fácil de tu software personalizado.'
+      },
+      'cleanScalableCodeTitle': {
+        'en': 'Clean & Scalable Code',
+        'es': 'Código Limpio y Escalable'
+      },
+      'cleanScalableCodeDesc': {
+        'en': 'We are committed to maintaining clean and structured code enabling continuous development and easy scalability of your custom software.',
+        'es': 'Nos comprometemos a mantener un código limpio y estructurado que permite un desarrollo continuo y una fácil escalabilidad de su software personalizado.'
+      },
+      'bespokeDevelopmentTitle': {
+        'en': 'Bespoke Development',
+        'es': 'Desarrollo A Medida'
+      },
+      'bespokeDevelopmentDesc': {
+        'en': 'Our personalized approach ensures that each solution is built specifically to meet your needs and goals, delivering precise and effective results.',
+        'es': 'Nuestro enfoque personalizado asegura que cada solución se construya específicamente para satisfacer sus necesidades y objetivos, entregando resultados precisos y efectivos.'
+      },
+      'dataProtectionSecurityTitle': {
+        'en': 'Comprehensive Data Protection and Security',
+        'es': 'Protección y Seguridad Integral de Datos'
+      },
+      'dataProtectionSecurityDesc': {
+        'en': 'We are committed to protecting your data and that of your clients with the most advanced security measures. With us, you can rest assured that your information is in good hands, allowing you to focus on what really matters: growing your business.',
+        'es': 'Estamos comprometidos con la protección de sus datos y los de sus clientes con las medidas de seguridad más avanzadas. Con nosotros, puede estar seguro de que su información está en buenas manos, permitiéndole concentrarse en lo que realmente importa: hacer crecer su negocio.'
+      },
+      'recentWorksTitle': {
+        'en': 'Recent Works',
+        'es': 'Trabajos Recientes'
+      },
+      'recentWorksDesc': {
+        'en': 'Each project represents our commitment to excellence and innovation. Explore our latest creations and discover how we have helped companies like yours achieve their online goals.',
+        'es': 'Cada proyecto representa nuestro compromiso con la excelencia y la innovación. Explora nuestras últimas creaciones y descubre cómo hemos ayudado a empresas como la tuya a alcanzar sus objetivos en línea.'
+      },
+      'teamMember':{
+        'en':'Team Members',
+        'es': 'Miembros del Equipo'
+      },
+      'getInTouchTitle': {
+        'en': 'Get In Touch',
+        'es': 'Ponte en Contacto'
+      },
+      'getInTouchDesc': {
+        'en': 'Get in touch and discover how we can help boost your business.',
+        'es': 'Ponte en contacto y descubre cómo podemos ayudar a impulsar tu negocio.'
+      },
+      'placeholderName': {
+        'en': 'Name',
+        'es': 'Nombre'
+      },
+      'placeholderSubject': {
+        'en': 'Subject',
+        'es': 'Asunto'
+      },
+      'placeholderEmail': {
+        'en': 'Email',
+        'es': 'Correo Electrónico'
+      },
+      'placeholderBudget': {
+        'en': 'Budget',
+        'es': 'Presupuesto'
+      },
+      'placeholderWriteMessage': {
+        'en': 'Write Message',
+        'es': 'Escribe un Mensaje'
+      },
+      'buttonSubmit': {
+        'en': 'Submit',
+        'es': 'Enviar'
+      },
+      'errorName': {
+        'en': 'Please enter your name',
+        'es': 'Por favor, ingrese su nombre'
+      },
+      'errorSubject': {
+        'en': 'Please enter your subject',
+        'es': 'Por favor, ingrese su asunto'
+      },
+      'errorEmail': {
+        'en': 'Please enter your Email',
+        'es': 'Por favor, ingrese su correo electrónico'
+      },
+      'errorBudget': {
+        'en': 'Please enter your Budget',
+        'es': 'Por favor, ingrese su presupuesto'
+      },
+      'errorMessage': {
+        'en': 'Write your message',
+        'es': 'Escriba su mensaje'
+      },
+      'formInvalid': {
+        'en': 'Did you fill in the form properly?',
+        'es': '¿Llenaste correctamente el formulario?'
+      },
+      'messageSubmitted': {
+        'en': 'Message Submitted!',
+        'es': '¡Mensaje Enviado!'
+      },
+  };
+  
+  function cambiarIdioma(lang) {
+    document.querySelectorAll('[data-key]').forEach(function(element) {
+      const key = element.getAttribute('data-key');
+      if (traducciones[key] && traducciones[key][lang]) {
+        if (element.tagName === 'INPUT' || element.tagName === 'TEXTAREA') {
+          element.setAttribute('placeholder', traducciones[key][lang]);
+        } else if (element.tagName === 'BUTTON') {
+          element.textContent = traducciones[key][lang];
+        } else {
+          element.innerHTML = traducciones[key][lang];
+        }
+      }
+    });
+  
+  // Actualizar los mensajes de error
+  document.querySelectorAll('[data-error-key]').forEach(function(element) {
+    const key = element.getAttribute('data-error-key');
+    if (traducciones[key] && traducciones[key][lang]) {
+      element.setAttribute('data-error', traducciones[key][lang]);
+    }
+  });
+    localStorage.setItem('selectedLanguage', lang);
+  }
+  
+  // Cargar el idioma almacenado al inicializar la página.
+  document.addEventListener('DOMContentLoaded', function() {
+    const storedLanguage = localStorage.getItem('selectedLanguage') || 'en';
+    cambiarIdioma(storedLanguage);
+    
+    // Actualizar el menú desplegable con el idioma seleccionado.
+    const languageDropdown = document.getElementById('languageDropdown');
+    languageDropdown.innerHTML = traducciones['idioma'][storedLanguage];
+  });
+  
+  
+  
+  
