b350aaca80139d1718327f7e2692f4cdf33c57d3
MEREmmanuel <informatica_21887037@cintalapa.tecnm.mx>
2024-04-19 22:17:35 -0600

    add environment config

--- php/form-process.php
+++ php/form-process.php
@@ -3,6 +3,16 @@ require("PHPMailer\PHPMailer.php");
 require("PHPMailer\SMTP.php");
 require("PHPMailer\Exception.php");
 
+// Ruta al archivo .env
+$envFile = dirname(__DIR__) . '/.env';
+
+// Leer el archivo .env y obtener las variables
+$envVariables = parse_ini_file($envFile);
+
+// Acceder a las variables de entorno
+$username = $envVariables['SMTP_USERNAME'];
+$password = $envVariables['SMTP_PASSWORD'];
+
 // Datos del formulario
 $name = $_POST['name'];
 $email = $_POST['email'];
@@ -16,15 +26,15 @@ $mail->IsSMTP(); // Habilitar SMTP
 $mail->SMTPDebug = 1; // No mostrar debugging
 $mail->SMTPAuth = true; // Habilitar autenticación SMTP
 $mail->SMTPSecure = 'ssl'; // Habilitar encriptación SSL
-$mail->Host = "smtp.titan.email";
+$mail->Host = "smtp.gmail.com";
 $mail->Port = 465; // o 587
 $mail->IsHTML(true);
-$mail->Username = "";
-$mail->Password = "";
+$mail->Username = $username;
+$mail->Password = $password;
 $mail->SetFrom($email, $name);
 $mail->Subject = $msg_subject;
 $mail->Body = "Nombre: $name <br>Email: $email <br>Asunto: $msg_subject <br>Presupuesto: $budget <br>Mensaje: $message";
-$mail->AddAddress("");
+$mail->AddAddress($username);
 $mail->addReplyTo($email, $name);
 
 // Enviar correo electrónico y manejar respuesta
